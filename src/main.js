74d504ef9df30b301bff3e3ff40fddca8cec4e18
conghui <740364177@qq.com>
2020-06-26 09:28:46 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入vant
+import vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(vant)
 
 Vue.config.productionTip = false
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,8 @@
 <template>
-<div>首页</div>
+<div>首页
+<van-button type="default">默认按钮</van-button>
+<van-button type="primary">主要按钮</van-button>
+</div>
 </template>
 
 <script>
