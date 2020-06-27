02dd4c7dc29959afc4eff7692884d40e6fbbb715
conghui <740364177@qq.com>
2020-06-27 16:19:28 +0800

    上拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -2,13 +2,15 @@
   <div class="scroll-wrapper">
     <!-- 文章列表  -->
     <!-- van-list 可以帮助我们实现上拉加载 需要一些变量 -->
+    <van-pull-refresh v-model="isRefresh" @refresh="onRefresh" :success-text="successText">
         <van-list v-model="upLoading" :finished="finished" @load="onLoad" finished-text="我是有底线的">
           <!-- 循环内容 -->
           <van-cell-group>
             <van-cell v-for="item in articles" :key="item" title="哈哈成功" :value="'排队'+item"></van-cell>
           </van-cell-group>
         </van-list>
-      </div>
+    </van-pull-refresh>
+   </div>
 </template>
 
 <script>
@@ -17,10 +19,13 @@ export default {
     return {
       upLoading: false,
       finished: false,
-      articles: []
+      articles: [],
+      isRefresh: false,
+      successText: ''
     }
   },
   methods: {
+    // 上拉加载
     onLoad () {
       console.log('加载数据')
       if (this.articles.length > 50) {
@@ -38,6 +43,14 @@ export default {
       //   // 这里用this， 用ES6函数
       //   this.finished = true
       // }, 1000)
+    },
+    // 下拉刷新
+    onRefresh () {
+      const arr = Array.from(Array(2), (value, index) => '追加' + (index + 1))
+      this.articles.unshift(...arr)
+      // 手动关闭加载
+      this.isRefresh = false
+      this.successText = `更新了${arr.length}条数据`
     }
   }
 }
