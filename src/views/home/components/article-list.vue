f2d3625a9f39e1a8c6b396078a415d15e245adc5
conghui <740364177@qq.com>
2020-06-28 00:38:21 +0800

    列表文章-布局

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -3,14 +3,35 @@
     <!-- 文章列表  -->
     <!-- van-list 可以帮助我们实现上拉加载 需要一些变量 -->
     <van-pull-refresh v-model="isRefresh" @refresh="onRefresh" :success-text="successText">
-        <van-list v-model="upLoading" :finished="finished" @load="onLoad" finished-text="我是有底线的">
-          <!-- 循环内容 -->
-          <van-cell-group>
-            <van-cell v-for="item in articles" :key="item" title="哈哈成功" :value="'排队'+item"></van-cell>
-          </van-cell-group>
-        </van-list>
+      <van-list v-model="upLoading" :finished="finished" @load="onLoad" finished-text="我是有底线的">
+        <!-- 循环内容 -->
+        <van-cell-group>
+          <van-cell v-for="item in articles" :key="item">
+            <div class="article_item">
+              <h3 class="van-ellipsis">这四个字，道出习近平的为民情怀</h3>
+              <div class="img_box">
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <!-- 一张图 -->
+              <!-- <div class="img_box">
+                <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div> -->
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+            </div>
+          </van-cell>
+        </van-cell-group>
+      </van-list>
     </van-pull-refresh>
-   </div>
+  </div>
 </template>
 
 <script>
@@ -31,8 +52,9 @@ export default {
       if (this.articles.length > 50) {
         this.finished = true // 关闭加载
       } else {
-        const arr = Array.from(Array(15), (value, index) =>
-          this.articles.length + index + 1
+        const arr = Array.from(
+          Array(15),
+          (value, index) => this.articles.length + index + 1
         )
         // 上拉加载，是把数据追加到数组结尾
         this.articles.push(...arr)
@@ -56,6 +78,45 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
