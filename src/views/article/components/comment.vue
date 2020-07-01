8fe824e29a94c46d1f1198419229fbde07708a84
conghui <740364177@qq.com>
2020-07-02 00:55:31 +0800

    文章详情-评论组件-回复列表布局

--- src/views/article/components/comment.vue
+++ src/views/article/components/comment.vue
@@ -24,14 +24,28 @@
           </p>
         </div>
       </div>
-
     </van-list>
+    <!-- 底部输入框 -->
     <div class="reply-container van-hairline--top">
       <van-field v-model="value" placeholder="写评论...">
         <van-loading v-if="submiting" slot="button" type="spinner" size="16px"></van-loading>
         <span class="submit" v-else slot="button">提交</span>
       </van-field>
     </div>
+    <!-- 放置评论的评论 弹出面板 -->
+ <!-- 回复 -->
+    <van-action-sheet v-model="showReply" :round="false" class="reply_dialog" title="回复评论">
+      <van-list v-model="reply.loading" :finished="reply.finished" finished-text="没有更多了">
+        <div class="item van-hairline--bottom van-hairline--top" v-for="index in 8" :key="index">
+          <van-image round width="1rem" height="1rem" fit="fill" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+          <div class="info">
+            <p><span class="name">一阵清风</span></p>
+            <p>评论的内容，。。。。</p>
+            <p><span class="time">两天内</span></p>
+          </div>
+        </div>
+      </van-list>
+    </van-action-sheet>
   </div>
 
   <!-- 都不输入框 -->
@@ -51,7 +65,12 @@ export default {
       // 控制提交中状态数据
       submiting: false,
       comments: [], // 所有评论
-      offset: null // 分页依据
+      offset: null, // 分页依据
+      showReply: false,
+      reply: {
+        loading: false,
+        finished: false
+      }
     }
   },
   methods: {
@@ -118,6 +137,25 @@ export default {
   .submit {
     font-size: 12px;
     color: #3296fa;
+  }
+}
+.reply_dialog {
+  height: 100%;
+  max-height: 100%;
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  .van-action-sheet__header {
+    background: #3296fa;
+    color: #fff;
+    .van-icon-close {
+      color: #fff;
+    }
+  }
+  .van-action-sheet__content{
+    flex: 1;
+    overflow-y: auto;
+    padding: 0 10px 44px;
   }
 }
 </style>
